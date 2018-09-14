4f5b169e105e27bdd054c2b8d21626bad4a6d726
epeuva <epeuva@gmail.com>
2018-09-14 14:08:06 +0200

    Added Follower entity and basic followers view (wip)

---
+++ spikeViperGithub/modules/followers-module/entity/Follower.swift
@@ -0,0 +1,17 @@
+//
+//  Follower.swift
+//  spikeViperGithub
+//
+//  Created by epeuva on 14/09/2018
+//  Copyright © 2018 epeuva. All rights reserved.
+//
+
+import Foundation
+
+//  See: https://developer.github.com/v3/users/followers/
+class Follower {
+    var id: Int?
+    var login: String?
+    var avatar_url: String?
+    var url: String?
+}
---
+++ spikeViperGithub/modules/followers-module/view/FollowersViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FollowersViewController.swift
+//  spikeViperGithub
+//
+//  Created by epeuva on 14/09/2018
+//  Copyright © 2018 epeuva. All rights reserved.
+//
+
+import UIKit
+
+protocol FollowersViewInterface: class {
+    func showFollowersData(followers: [Follower])
+    func showNoContentsScreen()
+}
+
+class FollowersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
