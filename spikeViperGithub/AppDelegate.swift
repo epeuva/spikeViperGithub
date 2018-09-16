6d86eb6cfde36d47b8928b06ca8a036d9ca1780a
Josep Peiro <epeuva@gmail.com>
2018-09-16 19:25:11 +0200

    Added Followers.storyboard. Added FollowersRouter and AppDelegate code in order to load Followers.storyboard first

--- spikeViperGithub/AppDelegate.swift
+++ spikeViperGithub/AppDelegate.swift
@@ -15,6 +15,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let followers = FollowersRouter.createModule()
+        
+        let navigationController = UINavigationController()
+        navigationController.viewControllers = [followers]
+        
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        
         // Override point for customization after application launch.
         return true
     }
--- spikeViperGithub/modules/followers-module/router/FollowersRouter.swift
+++ spikeViperGithub/modules/followers-module/router/FollowersRouter.swift
@@ -2,16 +2,36 @@
 //  FollowersRouter.swift
 //  spikeViperGithub
 //
-//  Created by Josep Peiro on 16/09/2018.
+//  Created by epeuva on 16/09/2018.
 //  Copyright © 2018 epeuva. All rights reserved.
 //
 
 import Foundation
+import UIKit
 
 class FollowersRouter: FollowersPresenterToRouterProtocol {
     
     static func createModule() -> FollowersViewController {
-        <#code#>
+        
+        // Take care the ViewController on Presenters.storyboard has the correct controller asigned
+        let view = mainstoryboard.instantiateViewController(withIdentifier: "MyFollowersViewController") as! FollowersViewController
+        
+        let presenter: FollowersViewToPresenterProtocol & FollowersInteractorToPresenterProtocol = FollowersPresenter()
+        let interactor: FollowersPresenterToInteractorProtocol = FollowersInteractor()
+        let router:FollowersPresenterToRouterProtocol = FollowersRouter()
+        
+        view.presenter = presenter
+        presenter.view = view
+        presenter.router = router
+        presenter.interactor = interactor
+        interactor.presenter = presenter
+        
+        return view
+        
     }
     
+    static var mainstoryboard: UIStoryboard{
+        // Take care Followers.storyboard exist
+        return UIStoryboard(name:"Followers",bundle: Bundle.main)
+    }
 }
