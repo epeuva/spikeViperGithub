8f11ba95c831b1478957d118707b31f62e3bdc7a
Josep Peiro <epeuva@gmail.com>
2018-09-16 19:03:45 +0200

    Added empty FollowersRouter

---
+++ spikeViperGithub/modules/followers-module/router/FollowersRouter.swift
@@ -0,0 +1,17 @@
+//
+//  FollowersRouter.swift
+//  spikeViperGithub
+//
+//  Created by Josep Peiro on 16/09/2018.
+//  Copyright © 2018 epeuva. All rights reserved.
+//
+
+import Foundation
+
+class FollowersRouter: FollowersPresenterToRouterProtocol {
+    
+    static func createModule() -> FollowersViewController {
+        <#code#>
+    }
+    
+}
